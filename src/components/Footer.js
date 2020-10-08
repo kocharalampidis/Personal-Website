7f538efc7df8e74c3460397539ca95d11c0df889
kocharalampidis <kocharalampidis@gmail.com>
2020-10-08 18:46:06 +0300

    added footer-header-css

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './css/Footer.css'
+
+function Footer(){
+    return (
+        <footer>
+            &copy; Copyright 2020
+        </footer>
+    )
+}
+
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/Header.css';
+
+
+function Header(){
+    return(
+        <h1>Hello World</h1>
+    )
+}
+
+export default Header
