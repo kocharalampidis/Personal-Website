a1deff14b61eddf61a472e9291bc02112e37e379
kocharalampidis <kocharalampidis@gmail.com>
2020-10-22 16:10:35 +0300

    responsive outer tables, have to fixe rows - cols to be responsive + bigger table and fonts

--- src/Pages/mybio.js
+++ src/Pages/mybio.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, {Fragment}     from 'react';
 import '../components/css/mybio.css'
 // http://localhost:3000/
 
@@ -9,20 +9,20 @@ function mybio(){
     <Fragment>    
         <body class="flex items-center justify-center">
             <div class="container">
-                <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+                <table class="">
                     <thead class="text-white">
-                        <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                        <tr class="bg-teal-400">
                                 <th class="p-3 text-left">Education</th>
                                 <th class="p-3 text-left">Timeline</th>
 
                         </tr>
                        </thead>
-                    <tbody class="flex-1 sm:flex-none">
-                        <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-                                <td class="border-grey-light border hover:bg-gray-100 p-3">Computer Science Department
-                                of the International Hellenic University, Software Engineer 
+                    <tbody class="">
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">Computer Science Department
+                                of the International Hellenic Univflex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5ersity, Software Engineer 
                                 </td>
-                                <td class="border-grey-light border hover:bg-gray-100 p-3"> 09/2013 - Σήμερα </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 "> 09/2013 - Σήμερα </td>
 
 
                         </tr>
@@ -33,7 +33,7 @@ function mybio(){
 
         <body class="flex items-center justify-center">
             <div class="container">
-                <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+                <table class=" flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
                     <thead class="text-white">
                         <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
                                 <th class="p-3 text-left">Work experience</th>
@@ -42,29 +42,109 @@ function mybio(){
                        </thead>
                     <tbody class="flex-1 sm:flex-none">
                         <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-                                <td class="border-grey-light border hover:bg-gray-100 p-3">Electrician Assistant, Prokat Kavalas </td>
-                                <td class="border-grey-light border hover:bg-gray-100 p-3"> 03/2014 - 10/2014 </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">Electrician Assistant, Prokat Kavalas </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 "> 03/2014 - 10/2014 </td>
 
                         </tr>
                         <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-                                <td class="border-grey-light border hover:bg-gray-100 p-3">Secretarial Support, Order and Warehouse Manager, 
-                                Nikolaos Manzouranis-Massage, Chiropractic and Alternative Therapies
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 "> Order and Warehouse Manager, 
+                                Nikolaos Manzouranis-Massage Chiropractic and Alternative Therapies
                                 </td>
-                                <td class="border-grey-light border hover:bg-gray-100 p-3">  11/2014 - 11/2015 </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">  11/2014 - 11/2015 </td>
 
                         </tr>
                         <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-                            <td class="border-grey-light border hover:bg-gray-100 p-3">Shift Manager, Tsalapeteinos Urban Farm</td>
-                            <td class="border-grey-light border hover:bg-gray-100 p-3"> 12/2015 - 07/2017 </td>
+                            <td class="border-grey-light border hover:bg-gray-100 p-3 ">Shift Manager, Tsalapeteinos Urban Farm</td>
+                            <td class="border-grey-light border hover:bg-gray-100 p-3 m"> 12/2015 - 07/2017 </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </body>
             
+
+
+        
+
     </Fragment>    
         
     );
 }
 
 export default mybio;
+
+
+
+
+{/* 
+
+
+
+ <body class="flex items-center justify-center">
+            <div class="container">
+                <table class="rounded-lg overflow-hidden max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-5xl xl:max-w-5xl my-5">
+                    <thead class="text-white">
+                        <tr class="bg-black">
+                                <th class="p-3 text-left">Education</th>
+                                <th class="p-3 text-left">Timeline</th>
+
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3  ">
+                                    Computer Science Department of the International Hersity, Software Engineer
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 "> 09/2013 - Σήμερα </td>
+
+
+                        </tr>
+                    </tbody>
+                </table>
+                
+                <table class="rounded-lg overflow-hidden max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-5xl xl:max-w-5xl my-5">
+                    <thead class="text-white">
+                        <tr class="bg-black">
+                                <th class="p-3 text-left">Work experience</th>
+                                <th class="p-3 text-left">Timeline</th>
+
+                        </tr>
+                       </thead>
+                    <tbody class="">
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    Electrician Assistant, Prokat Kavalas 
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                03/2014 - 10/2014
+                                </td>
+                            </tr>
+                            <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                Order and Warehouse Manager, Nikolaos Manzouranis-Massage Chiropractic and Alternative Therapies 
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                11/2014 - 11/2015 
+                                </td>
+                            </tr>
+                            <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                Shift Manager, Tsalapeteinos Urban Farm 
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    12/2015 - 07/2017
+                                </td>
+                        </tr>
+                    </tbody>
+                </table>
+            
+            </div>
+        </body>
+
+
+
+
+
+
+
+*/}
--- src/Pages/welcome.js
+++ src/Pages/welcome.js
@@ -1,9 +1,119 @@
-import React from 'react';
+import { faTable } from '@fortawesome/free-solid-svg-icons';
+import React, { Fragment } from 'react';
+import '../components/css/mybio.css'
+
 
 function welcome(){
-    return(
-        <h1>Hello World from welcome</h1>
+    return (
+    <Fragment>    
+      <body class="flex items-center justify-center">
+            <div class="container">
+
+
+                <table class="max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-xl xl:max-w-2xl my-5">
+                    <thead class="text-white">
+                        <tr class="bg-black">
+                                <th class="p-3 text-left">Education</th>
+                                <th class="p-3 text-left">Timeline</th>
+
+                        </tr>
+                       </thead>
+                    <tbody class="">
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3  ">
+                                    Computer Science Department
+                                    of the International Hellenic Univrsity, Software Engineer 
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 "> 09/2013 - Σήμερα </td>
+
+
+                        </tr>
+                    </tbody>
+                </table>
+
+                
+                <table class="max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-xl xl:max-w-2xl my-5">
+                    <thead class="text-white">
+                        <tr class="bg-black">
+                                <th class="p-3 text-left">Education</th>
+                                <th class="p-3 text-left">Timeline</th>
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                   
+                                    Electrician Assistant, Prokat Kavalas 
+
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    
+                                    03/2014 - 10/2014
+
+                                </td>
+                        </tr>
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    
+                                    Order and Warehouse Manager, Nikolaos Manzouranis-Massage Chiropractic and Alternative Therapies
+
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    11/2014 - 11/2015
+                                 </td>
+                        </tr>
+                        <tr class="">
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    
+                                    Shift Manager, Tsalapeteinos Urban Farm
+
+                                </td>
+                                <td class="border-grey-light border hover:bg-gray-100 p-3 ">
+                                    12/2015 - 07/2017
+                                </td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            
+            </div>
+        </body>
+            
+    </Fragment>    
+        
     );
 }
 
 export default welcome;
+
+// table  
+ 
+// thead 
+// th  
+// td  
+// tr  
+// tbody 
+
+{/* <body class="flex items-center justify-center">
+<div class="container">
+    <table class=" flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+        <thead class="text-white">
+            <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                    <th class="p-3 text-left">Education</th>
+                    <th class="p-3 text-left">Timeline</th>
+
+            </tr>
+           </thead>
+        <tbody class="flex-1 sm:flex-none">
+            <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 ">Computer Science Department
+                    of the International Hellenic University, Software Engineer 
+                    </td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 "> 09/2013 - Σήμερα </td>
+
+
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body> */}
