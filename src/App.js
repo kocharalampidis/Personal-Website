d041f9f843b88dc54a015d65a4a0dc8a78955297
kocharalampidis <kocharalampidis@gmail.com>
2020-10-26 20:14:40 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 // libraries imports
-import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // components/css import
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // pages imports
-import welcome from './Pages/welcome'
-import mybio from './Pages/mybio'
-import calendar from './Pages/calendar'
-
+import welcome from "./Pages/welcome";
+import mybio from "./Pages/mybio";
+import calendar from "./Pages/calendar";
 
 function App() {
-
-  
-  return (
-   
-    <div class="relative pb-10 border min-h-screen bg-red-900">
-      <BrowserRouter>
-      <Navbar/>
-          <div>
-            <Switch> 
-              <Redirect from="/" to="/welcome" exact />
-              <Route path='/welcome' component={welcome}/>
-              <Route path='/mybio' component={mybio}/>
-              <Route path='/calendar' component={calendar}/>
-            </Switch>
-          </div>
-        <Footer/>
-        </BrowserRouter>  
-      </div>
-    );
-  }
-
+	return (
+		<div class='relative pb-10 border min-h-screen bg-red-900'>
+			<BrowserRouter>
+				<Navbar />
+				<div>
+					<Switch>
+						<Redirect from='/' to='/welcome' exact />
+						<Route path='/welcome' component={welcome} />
+						<Route path='/mybio' component={mybio} />
+						<Route path='/calendar' component={calendar} />
+					</Switch>
+				</div>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
+}
 
 export default App;
 
