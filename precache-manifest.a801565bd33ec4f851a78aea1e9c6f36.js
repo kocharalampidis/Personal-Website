b050eea5409fa036d39cb73df43573cc651c6b9f
kocharalampidis <kocharalampidis@gmail.com>
2020-10-26 21:00:48 +0200

    Updates

---
+++ precache-manifest.a801565bd33ec4f851a78aea1e9c6f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed029e5cce914abbfd852fd576defa33",
+    "url": "/my-app/welcome/index.html"
+  },
+  {
+    "revision": "ab36984207685f0060d7",
+    "url": "/my-app/welcome/static/css/main.c06c1c41.chunk.css"
+  },
+  {
+    "revision": "9922e0b2570b6b5eac77",
+    "url": "/my-app/welcome/static/js/2.c255a257.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/my-app/welcome/static/js/2.c255a257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab36984207685f0060d7",
+    "url": "/my-app/welcome/static/js/main.e3e02e6f.chunk.js"
+  },
+  {
+    "revision": "ac81665bba00ffc0dfac",
+    "url": "/my-app/welcome/static/js/runtime-main.428fcd0c.js"
+  }
+]);
