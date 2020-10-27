25d83dbb115ffc33a67e56a72322201708fc5adb
kocharalampidis <kocharalampidis@gmail.com>
2020-10-27 11:02:43 +0200

    d

--- src/Pages/calendar.js
+++ src/Pages/calendar.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faDraftingCompass,
+	faRulerCombined,
+	faHardHat,
+	faTools,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-function caldendar(){
-    return(
-        <h1>  
-        <FontAwesomeIcon 
-            icon={faBars}
-            
-        />
-        </h1>
-    );
+function caldendar() {
+	return (
+		<div>
+			<div class='mt-12 text-center flex items-center justify-center'>
+				<h1 class='font-mono text-6xl font-black font'>Under Construction</h1>
+			</div>
+			<div class='mt-12 space-x-3  flex items-center justify-center'>
+				<FontAwesomeIcon icon={faDraftingCompass} size='7x' />
+				<FontAwesomeIcon icon={faRulerCombined} size='7x' />
+				<FontAwesomeIcon icon={faHardHat} size='7x' />
+				<FontAwesomeIcon icon={faTools} size='7x' />
+			</div>
+		</div>
+	);
 }
 
 export default caldendar;
--- src/Pages/mybio.js
+++ src/Pages/mybio.js
@@ -1,90 +1,72 @@
-import React, {Fragment}     from 'react';
-import '../components/css/mybio.css'
+import React, { Fragment } from "react";
+import "../components/css/mybio.css";
 // http://localhost:3000/
 
+function mybio() {
+	return (
+		<Fragment>
+			<body class='text-center flex items-center justify-center'>
+				<div class='container'>
+					<table class='rounded-lg overflow-hidden shadow-lg max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-xl xl:max-w-2xl my-5'>
+						<thead class='text-white'>
+							<tr class='bg-black'>
+								<th class='p-3 text-left'>Education</th>
+								<th class='p-3 text-left'>Timeline</th>
+							</tr>
+						</thead>
+						<tbody class=''>
+							<tr class=''>
+								<td class='border-green-600 border hover:bg-red-200 p-3  '>
+									Computer Science Department of the International Hellenic
+									Univrsity, Software Engineer
+								</td>
+								<td class='border-green-600 border hover:bg-red-200 p-3 '>
+									{" "}
+									09/2013 - Σήμερα{" "}
+								</td>
+							</tr>
+						</tbody>
+					</table>
 
-
-function mybio(){
-    return (
-        <Fragment>    
-        <body class="text-center flex items-center justify-center">
-              <div class="container">
-  
-  
-                  <table class="rounded-lg overflow-hidden shadow-lg max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-xl xl:max-w-2xl my-5">
-                      <thead class="text-white">
-                          <tr class="bg-black">
-                                  <th class="p-3 text-left">Education</th>
-                                  <th class="p-3 text-left">Timeline</th>
-  
-                          </tr>
-                         </thead>
-                      <tbody class="">
-                          <tr class="">
-                                  <td class="border-green-600 border hover:bg-red-200 p-3  ">
-                                      Computer Science Department
-                                      of the International Hellenic Univrsity, Software Engineer 
-                                  </td>
-                                  <td class="border-green-600 border hover:bg-red-200 p-3 "> 09/2013 - Σήμερα </td>
-  
-  
-                          </tr>
-                      </tbody>
-                  </table>
-  
-                  
-                  <table class="rounded-lg overflow-hidden shadow-lg max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-xl xl:max-w-2xl my-5">
-                      <thead class="text-white">
-                          <tr class="bg-black">
-                                  <th class="p-3 text-left">Work Experience</th>
-                                  <th class="p-3 text-left">Timeline</th>
-                          </tr>
-                      </thead>
-                      <tbody class="">
-                          <tr class="">
-                                  <td class="border-green-600 border hover:bg-red-200 p-3 ">
-                                     
-                                      Electrician Assistant, Prokat Kavalas 
-  
-                                  </td>
-                                  <td class="border-green-600 border hover:bg-red-200 p-3 ">
-                                      
-                                      03/2014 - 10/2014
-  
-                                  </td>
-                          </tr>
-                          <tr class="">
-                                  <td class="border-green-600  border hover:bg-red-200 p-3 ">
-                                      
-                                      Order and Warehouse Manager, Nikolaos Manzouranis-Massage Chiropractic and Alternative Therapies
-  
-                                  </td>
-                                  <td class="border-green-600 border hover:bg-red-200 p-3 ">
-                                      11/2014 - 11/2015
-                                   </td>
-                          </tr>
-                          <tr class="">
-                                  <td class="border-green-600 border hover:bg-red-200 p-3 ">
-                                      
-                                      Shift Manager, Tsalapeteinos Urban Farm
-  
-                                  </td>
-                                  <td class="border-green-600 border hover:bg-red-200 p-3 ">
-                                      12/2015 - 07/2017
-                                  </td>
-  
-                          </tr>
-                      </tbody>
-                  </table>
-              
-              </div>
-          </body>
-              
-      </Fragment>      
-        
-    );
+					<table class='rounded-lg overflow-hidden shadow-lg max-w-sm sm:max-w-xl md:max-w-xl lg:max-w-xl xl:max-w-2xl my-5'>
+						<thead class='text-white'>
+							<tr class='bg-black'>
+								<th class='p-3 text-left'>Work Experience</th>
+								<th class='p-3 text-left'>Timeline</th>
+							</tr>
+						</thead>
+						<tbody class=''>
+							<tr class=''>
+								<td class='border-green-600 border hover:bg-red-200 p-3 '>
+									Electrician Assistant, Prokat Kavalas
+								</td>
+								<td class='border-green-600 border hover:bg-red-200 p-3 '>
+									03/2014 - 10/2014
+								</td>
+							</tr>
+							<tr class=''>
+								<td class='border-green-600  border hover:bg-red-200 p-3 '>
+									Order and Warehouse Manager, Nikolaos Manzouranis-Massage
+									Chiropractic and Alternative Therapies
+								</td>
+								<td class='border-green-600 border hover:bg-red-200 p-3 '>
+									11/2014 - 11/2015
+								</td>
+							</tr>
+							<tr class=''>
+								<td class='border-green-600 border hover:bg-red-200 p-3 '>
+									Shift Manager, Tsalapeteinos Urban Farm
+								</td>
+								<td class='border-green-600 border hover:bg-red-200 p-3 '>
+									12/2015 - 07/2017
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</body>
+		</Fragment>
+	);
 }
 
 export default mybio;
-
-
