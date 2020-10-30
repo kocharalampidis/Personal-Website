741875194e657b70f3046b2582101919c6cb1c4a
kocharalampidis <kocharalampidis@gmail.com>
2020-10-30 09:45:38 +0200

    deleted calendar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,13 +26,6 @@ function Navigation() {
 						Projects
 					</Link>
 				</li>
-				<li class='mr-6'>
-					<Link
-						to='/Calendar'
-						class='ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'>
-						Calendar
-					</Link>
-				</li>
 			</ul>
 		</div>
 	);
