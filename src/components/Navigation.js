29ad96c023619bf98093f1fa70db9ad92922f595
kocharalampidis <kocharalampidis@gmail.com>
2020-11-07 10:40:33 +0200

    .

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
 		<div class=' lg:flex-grow'>
 			<Link
 				to='/'
-				class=' font-display font font-bold block mt-4 lg:inline-block lg:mt-0 px-3 py-2 rounded-md  leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out mr-4'>
+				class=' font-bold block mt-4 lg:inline-block lg:mt-0 px-3 py-2 rounded-md  leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out mr-4'>
 				Home
 			</Link>
 
