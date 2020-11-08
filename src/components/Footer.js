e19b32e68a968deee1a9ed8099e04039c8a0a896
kocharalampidis <kocharalampidis@gmail.com>
2020-11-08 12:08:33 +0200

    corrected footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,15 @@ import React from "react";
 
 function Footer() {
 	return (
-		<footer class='bg-gray-800 text-white absolute w-full bottom-0 border-t-4 border-white'>
-			<div class='text-left pl-6'>
-				<span class=' text-lg pt-1'> &copy; Copywright 2020 </span>
+		<footer class='bg-gray-800 absolute w-full bottom-0'>
+			<div class=' pl-6'>
+				<span class='text-white text-md font-semibold pr-4 border-r-2'>
+					{" Made By Konstantinos Charalampidis "}
+				</span>
+				<span class='text-white text-md pl-4  '>
+					{" "}
+					&copy; {" Copywright 2020 "}
+				</span>
 			</div>
 		</footer>
 	);
