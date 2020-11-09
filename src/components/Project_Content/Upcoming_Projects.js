0d68b4eb18e32200f5647f65b0c82bf5457ce1c4
kocharalampidis <kocharalampidis@gmail.com>
2020-11-09 09:57:37 +0200

    .

--- src/components/Project_Content/Upcoming_Projects.js
+++ src/components/Project_Content/Upcoming_Projects.js
@@ -5,7 +5,7 @@ import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 function Upcoming_Projects() {
 	return (
 		<>
-			<body class=' h-screen overflow-hidden flex items-center justify-center border-dashed border-4 border-black'>
+			<body class=' h-screen overflow-hidden flex items-center justify-center border-dashed border-4 border-white border-opacity-50'>
 				<div class='lg:flex shadow  bg-green-600 '>
 					<div class='lg:w-2/12 py-4 block h-full'>
 						<div class=''></div>
@@ -24,7 +24,7 @@ function Upcoming_Projects() {
 							{"Basic Dialog Interactions, Personal Assistant"}
 						</div>
 					</div>
-					<div class='flex flex-row items-center w-full lg:w-1/3 bg-white lg:justify-end justify-center px-2 py-4 lg:px-0'>
+					<div class='flex flex-row items-center w-full lg:w-1/3 bg-white  lg:justify-end justify-center px-2 py-4 lg:px-0'>
 						<span class='tracking-wider text-gray-600 bg-gray-200 px-2 text-sm rounded leading-loose mx-2 font-semibold'>
 							{"Doing Research"}
 						</span>
